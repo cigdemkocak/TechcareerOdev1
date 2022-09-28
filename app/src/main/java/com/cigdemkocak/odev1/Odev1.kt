2db449b880a46a85af037fab29876b62f0b43442
Çiğdem Koçak <kocakcigdem9@gmail.com>
2022-09-28 14:15:00 +0300

    first commit

---
+++ app/src/main/java/com/cigdemkocak/odev1/Odev1.kt
@@ -0,0 +1,54 @@
+package com.cigdemkocak.odev1
+
+fun main() {
+    //islem
+    var islemNo = 111
+    var ogrNo = 112
+    var kitapNo = 113
+    var aTarih = "28.09.2022"
+    var vTarih = "29.09.2022"
+
+    //ogrenci
+    var ogrAd = "Çiğdem"
+    var ogrSoyad = "Koçak"
+    var cinsiyet = "Kadın"
+    var dTarih = "29.06.1997"
+    var sinif = 4
+
+    //kitap
+    var isbnno = 115
+    var kitapAdi = "Kitap Adı"
+    var yazarNo = 116
+    var turNo = 117
+    var sayfaSayisi = 118
+    var puan = 119
+
+    //yazar
+    var yazarAd = "Yazar Ad"
+    var yazarSoyad = "Yazar Soyad"
+
+    //tür
+    var turAdi = "Tür Adı"
+
+    println(islemNo)
+    println(ogrNo)
+    println(kitapNo)
+    println(aTarih)
+    println(vTarih)
+    println(ogrAd)
+    println(ogrSoyad)
+    println(cinsiyet)
+    println(dTarih)
+    println(sinif)
+    println(isbnno)
+    println(kitapAdi)
+    println(yazarNo)
+    println(turNo)
+    println(sayfaSayisi)
+    println(puan)
+    println(yazarAd)
+    println(yazarSoyad)
+    println(turAdi)
+
+
+}
